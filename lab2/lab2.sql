deed21f94c33683872c9bca950e28c16b781c43b
Angelina Rusinovich <lina.rusinovich@mail.ru>
2021-09-06 10:40:00 +0300

    Added solution for sql-tasks

---
+++ lab2/lab2.sql
@@ -0,0 +1,52 @@
+/* Task ¹ 2 */
+/* Select all information about genres*/
+
+SELECT * 
+FROM [genres]
+
+
+/* Task ¹ 3 */
+/* Show ids (without dublication) of the books that were taken by the readers. */
+
+SELECT DISTINCT [sb_book]
+FROM [subscriptions]
+
+
+/* Task ¹ 12 */
+/* Show id of one (any) reader who has borrowed the most books from the library. */
+
+SELECT TOP 1 [sb_subscriber]
+FROM [subscriptions]
+GROUP BY [sb_subscriber]
+ORDER BY COUNT(*) DESC
+
+
+/* Task ¹ 16 */
+/* Show in days how many, on average, readers have 
+already been registered in the library. */
+
+SELECT AVG(DATEDIFF(day, [sb_start], GETDATE())) AS [avg_days_register]
+FROM [subscriptions] AS [outer]
+WHERE [sb_id] = (SELECT TOP 1 [sb_id]
+	FROM [subscriptions] AS [inner]
+	WHERE [outer].[sb_subscriber] = [inner].[sb_subscriber]
+	ORDER BY [sb_start] ASC) 
+
+
+
+/* Task ¹ 17 */
+/* Show how many books were returned and not returned to the library. */
+
+SELECT (CASE
+	WHEN [sb_is_active] = 'Y'
+	THEN 'Not returned'
+	ELSE 'Returned'
+	END) AS [status],
+	COUNT([sb_id]) AS [amount_books]
+FROM [subscriptions]
+GROUP BY (CASE
+	WHEN [sb_is_active] = 'Y'
+	THEN 'Not returned'
+	ELSE 'Returned'
+	END)
+ORDER BY [status] DESC
---
+++ lab4/lab4.sql
@@ -0,0 +1,66 @@
+/* Task № 1 */
+/* Add information about three new readers to the database: 
+"Орлов O.O.", "Соколов С.С.", "Беркутов Б.Б.". */
+
+INSERT INTO [subscribers] ([s_name])
+VALUES
+	(N'Орлов О.О.'),
+	(N'Соколов С.С.'),
+	(N'Беркутов Б.Б.');
+
+/* OR */
+
+SET IDENTITY_INSERT [subscribers] ON;
+INSERT INTO [subscribers] ([s_id], [s_name])
+VALUES
+	(5, N'Орлов О.О.'),	
+	(6, N'Соколов С.С.'),
+	(7, N'Беркутов Б.Б.');
+SET IDENTITY_INSERT [subscribers] OFF;
+
+
+/* Task № 7 */
+/* Delete information about all issuances to readers of the book with ID = 1. */
+
+UPDATE [subscriptions]
+SET [sb_is_active] = N'N'
+WHERE [sb_id] <= 50
+SELECT * FROM [subscriptions]
+
+
+/* Task № 7 */
+/* Delete information about all issuances to readers of the book with ID = 1. */
+
+DELETE FROM [subscriptions]
+WHERE [sb_book] = 1
+SELECT * FROM [subscriptions]
+
+
+/* Task № 9 */
+/* Delete information about all book loans made after the 
+20th day of any month of any year. */
+
+DELETE FROM [subscriptions]
+WHERE DAY([sb_start]) > 20
+
+
+/* Task № 13 */
+/* Update all the names of the authors, I added the names "[+]" to the 
+branch if there is a sick book by this author in the library, or added 
+the names "[-]" to the branch in the opposite case */
+
+WITH [prepared_data]
+	AS (SELECT [a_id], COUNT([b_id]) AS [book_count]
+		FROM [m2m_books_authors]
+		GROUP BY [a_id])
+
+UPDATE [authors]
+SET [a_name] =
+	(SELECT
+		CASE
+		WHEN [book_count] > 3
+		THEN (SELECT CONCAT([a_name], ' [+] '))
+		ELSE (SELECT CONCAT([a_name], ' [-] '))
+		END
+	FROM [prepared_data]
+	WHERE [authors].[a_id] = [prepared_data].[a_id])
